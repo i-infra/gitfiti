c8ecccd6b354f4cf83d005e03f55d90791a7b036
i-infra <77526490+i-infra@users.noreply.github.com>
2024-12-04 19:45:32 -0500

    black

--- gitfiti.py
+++ gitfiti.py
@@ -14,6 +14,7 @@ import itertools
 import json
 import math
 import os
+
 try:
     # Python 3+
     from urllib.error import HTTPError, URLError
@@ -30,174 +31,176 @@ except NameError:
     raw_input = input
 
 
-GITHUB_BASE_URL = 'https://github.com/'
-FALLBACK_IMAGE = 'kitty'
+GITHUB_BASE_URL = "https://github.com/"
+FALLBACK_IMAGE = "kitty"
 
 
-TITLE = '''
+TITLE = """
           _ __  _____ __  _
    ____ _(_) /_/ __(_) /_(_)
   / __ `/ / __/ /_/ / __/ /
  / /_/ / / /_/ __/ / /_/ /
  \__, /_/\__/_/ /_/\__/_/
 /____/
-'''
+"""
 
 
 KITTY = [
-  [0,0,0,4,0,0,0,0,4,0,0,0],
-  [0,0,4,2,4,4,4,4,2,4,0,0],
-  [0,0,4,2,2,2,2,2,2,4,0,0],
-  [2,2,4,2,4,2,2,4,2,4,2,2],
-  [0,0,4,2,2,3,3,2,2,4,0,0],
-  [2,2,4,2,2,2,2,2,2,4,2,2],
-  [0,0,0,3,4,4,4,4,3,0,0,0],
+    [0, 0, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0],
+    [0, 0, 4, 2, 4, 4, 4, 4, 2, 4, 0, 0],
+    [0, 0, 4, 2, 2, 2, 2, 2, 2, 4, 0, 0],
+    [2, 2, 4, 2, 4, 2, 2, 4, 2, 4, 2, 2],
+    [0, 0, 4, 2, 2, 3, 3, 2, 2, 4, 0, 0],
+    [2, 2, 4, 2, 2, 2, 2, 2, 2, 4, 2, 2],
+    [0, 0, 0, 3, 4, 4, 4, 4, 3, 0, 0, 0],
 ]
 
 ONEUP = [
-  [0,4,4,4,4,4,4,4,0],
-  [4,3,2,2,1,2,2,3,4],
-  [4,2,2,1,1,1,2,2,4],
-  [4,3,4,4,4,4,4,3,4],
-  [4,4,1,4,1,4,1,4,4],
-  [0,4,1,1,1,1,1,4,0],
-  [0,0,4,4,4,4,4,0,0],
+    [0, 4, 4, 4, 4, 4, 4, 4, 0],
+    [4, 3, 2, 2, 1, 2, 2, 3, 4],
+    [4, 2, 2, 1, 1, 1, 2, 2, 4],
+    [4, 3, 4, 4, 4, 4, 4, 3, 4],
+    [4, 4, 1, 4, 1, 4, 1, 4, 4],
+    [0, 4, 1, 1, 1, 1, 1, 4, 0],
+    [0, 0, 4, 4, 4, 4, 4, 0, 0],
 ]
 
 ONEUP2 = [
-  [0,0,4,4,4,4,4,4,4,0,0],
-  [0,4,2,2,1,1,1,2,2,4,0],
-  [4,3,2,2,1,1,1,2,2,3,4],
-  [4,3,3,4,4,4,4,4,3,3,4],
-  [0,4,4,1,4,1,4,1,4,4,0],
-  [0,0,4,1,1,1,1,1,4,0,0],
-  [0,0,0,4,4,4,4,4,0,0,0],
+    [0, 0, 4, 4, 4, 4, 4, 4, 4, 0, 0],
+    [0, 4, 2, 2, 1, 1, 1, 2, 2, 4, 0],
+    [4, 3, 2, 2, 1, 1, 1, 2, 2, 3, 4],
+    [4, 3, 3, 4, 4, 4, 4, 4, 3, 3, 4],
+    [0, 4, 4, 1, 4, 1, 4, 1, 4, 4, 0],
+    [0, 0, 4, 1, 1, 1, 1, 1, 4, 0, 0],
+    [0, 0, 0, 4, 4, 4, 4, 4, 0, 0, 0],
 ]
 
 HACKERSCHOOL = [
-  [4,4,4,4,4,4],
-  [4,3,3,3,3,4],
-  [4,1,3,3,1,4],
-  [4,3,3,3,3,4],
-  [4,4,4,4,4,4],
-  [0,0,4,4,0,0],
-  [4,4,4,4,4,4],
+    [4, 4, 4, 4, 4, 4],
+    [4, 3, 3, 3, 3, 4],
+    [4, 1, 3, 3, 1, 4],
+    [4, 3, 3, 3, 3, 4],
+    [4, 4, 4, 4, 4, 4],
+    [0, 0, 4, 4, 0, 0],
+    [4, 4, 4, 4, 4, 4],
 ]
 
 OCTOCAT = [
-  [0,0,0,4,0,0,0,4,0],
-  [0,0,4,4,4,4,4,4,4],
-  [0,0,4,1,3,3,3,1,4],
-  [4,0,3,4,3,3,3,4,3],
-  [0,4,0,0,4,4,4,0,0],
-  [0,0,4,4,4,4,4,4,4],
-  [0,0,4,0,4,0,4,0,4],
+    [0, 0, 0, 4, 0, 0, 0, 4, 0],
+    [0, 0, 4, 4, 4, 4, 4, 4, 4],
+    [0, 0, 4, 1, 3, 3, 3, 1, 4],
+    [4, 0, 3, 4, 3, 3, 3, 4, 3],
+    [0, 4, 0, 0, 4, 4, 4, 0, 0],
+    [0, 0, 4, 4, 4, 4, 4, 4, 4],
+    [0, 0, 4, 0, 4, 0, 4, 0, 4],
 ]
 
 OCTOCAT2 = [
-  [0,0,4,0,0,4,0],
-  [0,4,4,4,4,4,4],
-  [0,4,1,3,3,1,4],
-  [0,4,4,4,4,4,4],
-  [4,0,0,4,4,0,0],
-  [0,4,4,4,4,4,0],
-  [0,0,0,4,4,4,0],
+    [0, 0, 4, 0, 0, 4, 0],
+    [0, 4, 4, 4, 4, 4, 4],
+    [0, 4, 1, 3, 3, 1, 4],
+    [0, 4, 4, 4, 4, 4, 4],
+    [4, 0, 0, 4, 4, 0, 0],
+    [0, 4, 4, 4, 4, 4, 0],
+    [0, 0, 0, 4, 4, 4, 0],
 ]
 
 HELLO = [
-  [0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,4],
-  [0,2,0,0,0,0,0,0,0,2,0,2,0,0,0,0,0,4],
-  [0,3,3,3,0,2,3,3,0,3,0,3,0,1,3,1,0,3],
-  [0,4,0,4,0,4,0,4,0,4,0,4,0,4,0,4,0,3],
-  [0,3,0,3,0,3,3,3,0,3,0,3,0,3,0,3,0,2],
-  [0,2,0,2,0,2,0,0,0,2,0,2,0,2,0,2,0,0],
-  [0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,4],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 4],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 4],
+    [0, 3, 3, 3, 0, 2, 3, 3, 0, 3, 0, 3, 0, 1, 3, 1, 0, 3],
+    [0, 4, 0, 4, 0, 4, 0, 4, 0, 4, 0, 4, 0, 4, 0, 4, 0, 3],
+    [0, 3, 0, 3, 0, 3, 3, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 2],
+    [0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0],
+    [0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 4],
 ]
 
 HEART1 = [
-  [0,1,1,0,1,1,0],
-  [1,3,3,1,3,3,1],
-  [1,3,4,3,4,3,1],
-  [1,3,4,4,4,3,1],
-  [0,1,3,4,3,1,0],
-  [0,0,1,3,1,0,0],
-  [0,0,0,1,0,0,0],        
+    [0, 1, 1, 0, 1, 1, 0],
+    [1, 3, 3, 1, 3, 3, 1],
+    [1, 3, 4, 3, 4, 3, 1],
+    [1, 3, 4, 4, 4, 3, 1],
+    [0, 1, 3, 4, 3, 1, 0],
+    [0, 0, 1, 3, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
 ]
 
 HEART2 = [
-  [0,5,5,0,5,5,0],
-  [5,3,3,5,3,3,5],
-  [5,3,1,3,1,3,5],
-  [5,3,1,1,1,3,5],
-  [0,5,3,1,3,5,0],
-  [0,0,5,3,5,0,0],
-  [0,0,0,5,0,0,0],        
+    [0, 5, 5, 0, 5, 5, 0],
+    [5, 3, 3, 5, 3, 3, 5],
+    [5, 3, 1, 3, 1, 3, 5],
+    [5, 3, 1, 1, 1, 3, 5],
+    [0, 5, 3, 1, 3, 5, 0],
+    [0, 0, 5, 3, 5, 0, 0],
+    [0, 0, 0, 5, 0, 0, 0],
 ]
 
 HIREME = [
-  [1,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [3,3,3,0,2,0,3,3,3,0,2,3,3,0,0,3,3,0,3,0,0,2,3,3],
-  [4,0,4,0,4,0,4,0,0,0,4,0,4,0,0,4,0,4,0,4,0,4,0,4],
-  [3,0,3,0,3,0,3,0,0,0,3,3,3,0,0,3,0,3,0,3,0,3,3,3],
-  [2,0,2,0,2,0,2,0,0,0,2,0,0,0,0,2,0,2,0,2,0,2,0,0],
-  [1,0,1,0,1,0,1,0,0,0,1,1,1,0,0,1,0,1,0,1,0,1,1,1],
+    [1, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [3, 3, 3, 0, 2, 0, 3, 3, 3, 0, 2, 3, 3, 0, 0, 3, 3, 0, 3, 0, 0, 2, 3, 3],
+    [4, 0, 4, 0, 4, 0, 4, 0, 0, 0, 4, 0, 4, 0, 0, 4, 0, 4, 0, 4, 0, 4, 0, 4],
+    [3, 0, 3, 0, 3, 0, 3, 0, 0, 0, 3, 3, 3, 0, 0, 3, 0, 3, 0, 3, 0, 3, 3, 3],
+    [2, 0, 2, 0, 2, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 2, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
 ]
 
 BEER = [
-  [0,0,0,0,0,0,0,3,3,3,0,0,3,3,3,0,3,3,3,0,3,3,3,0,0],
-  [0,0,1,1,1,1,0,3,0,0,3,0,3,0,0,0,3,0,0,0,3,0,0,3,0],
-  [0,2,2,2,2,2,0,3,0,0,3,0,3,0,0,0,3,0,0,0,3,0,0,3,0],
-  [2,0,2,2,2,2,0,3,3,3,0,0,3,3,3,0,3,3,3,0,3,3,3,0,0],
-  [2,0,2,2,2,2,0,3,0,0,3,0,3,0,0,0,3,0,0,0,3,0,3,0,0],
-  [0,2,2,2,2,2,0,3,0,0,3,0,3,0,0,0,3,0,0,0,3,0,0,3,0],
-  [0,0,2,2,2,2,0,3,3,3,0,0,3,3,3,0,3,3,3,0,3,0,0,3,0],
+    [0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 3, 3, 3, 0, 0],
+    [0, 0, 1, 1, 1, 1, 0, 3, 0, 0, 3, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 3, 0],
+    [0, 2, 2, 2, 2, 2, 0, 3, 0, 0, 3, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 3, 0],
+    [2, 0, 2, 2, 2, 2, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 3, 3, 3, 0, 0],
+    [2, 0, 2, 2, 2, 2, 0, 3, 0, 0, 3, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 3, 0, 0],
+    [0, 2, 2, 2, 2, 2, 0, 3, 0, 0, 3, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 3, 0],
+    [0, 0, 2, 2, 2, 2, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 3, 0, 0, 3, 0],
 ]
 
 GLIDERS = [
-  [0,0,0,4,0,4,0,0,0,0,4,0,0,0],
-  [0,4,0,4,0,0,4,4,0,0,0,4,0,0],
-  [0,0,4,4,0,4,4,0,0,4,4,4,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,4,0,4,0,0,0,4,0,0,0,0,0,0],
-  [0,0,4,4,0,4,0,4,0,0,0,0,0,0],
-  [0,0,4,0,0,0,4,4,0,0,0,0,0,0],
+    [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4, 0, 0, 0],
+    [0, 4, 0, 4, 0, 0, 4, 4, 0, 0, 0, 4, 0, 0],
+    [0, 0, 4, 4, 0, 4, 4, 0, 0, 4, 4, 4, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 4, 0, 4, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 4, 0, 4, 0, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 4, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
 ]
 
 HEART = [
-  [0,4,4,0,4,4,0],
-  [4,2,2,4,2,2,4],
-  [4,2,2,2,2,2,4],
-  [4,2,2,2,2,2,4],
-  [0,4,2,2,2,4,0],
-  [0,0,4,2,4,0,0],
-  [0,0,0,4,0,0,0],
+    [0, 4, 4, 0, 4, 4, 0],
+    [4, 2, 2, 4, 2, 2, 4],
+    [4, 2, 2, 2, 2, 2, 4],
+    [4, 2, 2, 2, 2, 2, 4],
+    [0, 4, 2, 2, 2, 4, 0],
+    [0, 0, 4, 2, 4, 0, 0],
+    [0, 0, 0, 4, 0, 0, 0],
 ]
 
 HEART_SHINY = [
-  [0,4,4,0,4,4,0],
-  [4,2,0,4,2,2,4],
-  [4,0,2,2,2,2,4],
-  [4,2,2,2,2,2,4],
-  [0,4,2,2,2,4,0],
-  [0,0,4,2,4,0,0],
-  [0,0,0,4,0,0,0],
+    [0, 4, 4, 0, 4, 4, 0],
+    [4, 2, 0, 4, 2, 2, 4],
+    [4, 0, 2, 2, 2, 2, 4],
+    [4, 2, 2, 2, 2, 2, 4],
+    [0, 4, 2, 2, 2, 4, 0],
+    [0, 0, 4, 2, 4, 0, 0],
+    [0, 0, 0, 4, 0, 0, 0],
 ]
 
 ASCII_TO_NUMBER = {
-  '_': 0,
-  '_': 1,
-  '~': 2,
-  '=': 3,
-  '*': 4,
+    "_": 0,
+    "_": 1,
+    "~": 2,
+    "=": 3,
+    "*": 4,
 }
 
 
 def str_to_sprite(content):
     # Break out lines and filter any excess
-    lines = content.split('\n')
+    lines = content.split("\n")
+
     def is_empty_line(line):
         return len(line) != 0
+
     lines = filter(is_empty_line, lines)
 
     # Break up lines into each character
@@ -212,7 +215,8 @@ def str_to_sprite(content):
     return split_lines
 
 
-ONEUP_STR = str_to_sprite('''
+ONEUP_STR = str_to_sprite(
+    """
  *******
 *=~~-~~=*
 *~~---~~*
@@ -220,55 +224,57 @@ ONEUP_STR = str_to_sprite('''
 **-*-*-**
  *-----*
   *****
-''')
+"""
+)
 
 
 IMAGES = {
-  'kitty': KITTY,
-  'oneup': ONEUP,
-  'oneup2': ONEUP2,
-  'hackerschool': HACKERSCHOOL,
-  'octocat': OCTOCAT,
-  'octocat2': OCTOCAT2,
-  'hello': HELLO,
-  'heart1': HEART1,
-  'heart2': HEART2,
-  'hireme': HIREME,
-  'oneup_str': ONEUP_STR,
-  'beer': BEER,
-  'gliders': GLIDERS,
-  'heart' : HEART, 
-  'heart_shiny' : HEART_SHINY,
+    "kitty": KITTY,
+    "oneup": ONEUP,
+    "oneup2": ONEUP2,
+    "hackerschool": HACKERSCHOOL,
+    "octocat": OCTOCAT,
+    "octocat2": OCTOCAT2,
+    "hello": HELLO,
+    "heart1": HEART1,
+    "heart2": HEART2,
+    "hireme": HIREME,
+    "oneup_str": ONEUP_STR,
+    "beer": BEER,
+    "gliders": GLIDERS,
+    "heart": HEART,
+    "heart_shiny": HEART_SHINY,
 }
 
 SHELLS = {
-  'bash': 'sh',
-  'powershell': 'ps1',
+    "bash": "sh",
+    "powershell": "ps1",
 }
+
 
 def load_images(img_names):
     """loads user images from given file(s)"""
-    if img_names[0] == '':
+    if img_names[0] == "":
         return {}
 
     for image_name in img_names:
         with open(image_name) as img:
             loaded_imgs = {}
-            img_list = ''
-            img_line = ' '
-            name = img.readline().replace('\n', '')
+            img_list = ""
+            img_line = " "
+            name = img.readline().replace("\n", "")
             name = name[1:]
 
             while True:
                 img_line = img.readline()
-                if img_line == '':
+                if img_line == "":
                     break
 
-                img_line.replace('\n', '')
-                if img_line[0] == ':':
+                img_line.replace("\n", "")
+                if img_line[0] == ":":
                     loaded_imgs[name] = json.loads(img_list)
                     name = img_line[1:]
-                    img_list = ''
+                    img_list = ""
                 else:
                     img_list += img_line
 
@@ -279,17 +285,17 @@ def load_images(img_names):
 
 def retrieve_contributions_calendar(username, base_url):
     """retrieves the GitHub commit calendar data for a username"""
-    base_url = base_url + 'users/' + username
+    base_url = base_url + "users/" + username
 
     try:
-        url = base_url + '/contributions'
+        url = base_url + "/contributions"
         page = urlopen(url)
     except (HTTPError, URLError) as e:
-        print('There was a problem fetching data from {0}'.format(url))
+        print("There was a problem fetching data from {0}".format(url))
         print(e)
         raise SystemExit
 
-    return page.read().decode('utf-8')
+    return page.read().decode("utf-8")
 
 
 def parse_contributions_calendar(contributions_calendar):
@@ -297,8 +303,8 @@ def parse_contributions_calendar(contributions_calendar):
     for line in contributions_calendar.splitlines():
         # a valid line looks like this:
         # <rect width="11" height="11" x="-31" y="0" class="ContributionCalendar-day" data-date="2023-02-26" data-level="3" rx="2" ry="2">23 contributions on Sunday, February 26, 2023</rect>
-        if 'data-date=' in line:
-            commit = line.split('>')[1].split()[0] # yuck
+        if "data-date=" in line:
+            commit = line.split(">")[1].split()[0]  # yuck
 
             if commit.isnumeric():
                 yield int(commit)
@@ -355,72 +361,74 @@ def generate_values_in_date_order(image, multiplier=1):
 
 def commit(commitdate, shell):
     template_bash = (
-        '''GIT_AUTHOR_DATE={0} GIT_COMMITTER_DATE={1} '''
-        '''git commit --allow-empty -m "gitfiti" > /dev/null\n'''
-    )
-    
-    template_powershell = (
-        '''$Env:GIT_AUTHOR_DATE="{0}"\n$Env:GIT_COMMITTER_DATE="{1}"\n'''
-        '''git commit --allow-empty -m "gitfiti" | Out-Null\n'''
+        """GIT_AUTHOR_DATE={0} GIT_COMMITTER_DATE={1} """
+        """git commit --allow-empty -m "gitfiti" > /dev/null\n"""
     )
 
-    template = template_bash if shell == 'bash' else template_powershell
+    template_powershell = (
+        """$Env:GIT_AUTHOR_DATE="{0}"\n$Env:GIT_COMMITTER_DATE="{1}"\n"""
+        """git commit --allow-empty -m "gitfiti" | Out-Null\n"""
+    )
+
+    template = template_bash if shell == "bash" else template_powershell
 
     return template.format(commitdate.isoformat(), commitdate.isoformat())
 
 
 def fake_it(image, start_date, username, repo, git_url, shell, offset=0, multiplier=1):
     template_bash = (
-        '#!/usr/bin/env bash\n'
-        'REPO={0}\n'
-        'git init $REPO\n'
-        'cd $REPO\n'
-        'touch README.md\n'
-        'git add README.md\n'
-        'touch gitfiti\n'
-        'git add gitfiti\n'
-        '{1}\n'
-        'git branch -M main\n'
-        'git remote add origin {2}:{3}/$REPO.git\n'
-        'git pull origin main\n'
-        'git push -u origin main\n'
+        "#!/usr/bin/env bash\n"
+        "REPO={0}\n"
+        "git init $REPO\n"
+        "cd $REPO\n"
+        "touch README.md\n"
+        "git add README.md\n"
+        "touch gitfiti\n"
+        "git add gitfiti\n"
+        "{1}\n"
+        "git branch -M main\n"
+        "git remote add origin {2}:{3}/$REPO.git\n"
+        "git pull origin main\n"
+        "git push -u origin main\n"
     )
 
     template_powershell = (
-        'cd $PSScriptRoot\n'
+        "cd $PSScriptRoot\n"
         '$REPO="{0}"\n'
-        'git init $REPO\n'
-        'cd $REPO\n'
-        'New-Item README.md -ItemType file | Out-Null\n'
-        'git add README.md\n'
-        'New-Item gitfiti -ItemType file | Out-Null\n'
-        'git add gitfiti\n'
-        '{1}\n'
-        'git branch -M main\n'
-        'git remote add origin {2}:{3}/$REPO.git\n'
-        'git pull origin main\n'
-        'git push -u origin main\n'
+        "git init $REPO\n"
+        "cd $REPO\n"
+        "New-Item README.md -ItemType file | Out-Null\n"
+        "git add README.md\n"
+        "New-Item gitfiti -ItemType file | Out-Null\n"
+        "git add gitfiti\n"
+        "{1}\n"
+        "git branch -M main\n"
+        "git remote add origin {2}:{3}/$REPO.git\n"
+        "git pull origin main\n"
+        "git push -u origin main\n"
     )
 
-    template = template_bash if shell == 'bash' else template_powershell
+    template = template_bash if shell == "bash" else template_powershell
 
     strings = []
-    for value, date in zip(generate_values_in_date_order(image, multiplier),
-            generate_next_dates(start_date, offset)):
+    for value, date in zip(
+        generate_values_in_date_order(image, multiplier),
+        generate_next_dates(start_date, offset),
+    ):
         for _ in range(value):
             strings.append(commit(date, shell))
 
-    return template.format(repo, ''.join(strings), git_url, username)
+    return template.format(repo, "".join(strings), git_url, username)
 
 
 def save(output, filename):
     """Saves the list to a given filename"""
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         f.write(output)
-    os.chmod(filename, 0o755) # add execute permissions
+    os.chmod(filename, 0o755)  # add execute permissions
 
 
-def request_user_input(prompt='> '):
+def request_user_input(prompt="> "):
     """Request input from the user and return what has been entered."""
     return raw_input(prompt)
 
@@ -429,9 +437,10 @@ def main():
     print(TITLE)
 
     ghe = request_user_input(
-        'Enter GitHub URL (leave blank to use {}): '.format(GITHUB_BASE_URL))
+        "Enter GitHub URL (leave blank to use {}): ".format(GITHUB_BASE_URL)
+    )
 
-    username = request_user_input('Enter your GitHub username: ')
+    username = request_user_input("Enter your GitHub username: ")
 
     git_base = ghe if ghe else GITHUB_BASE_URL
 
@@ -441,36 +450,38 @@ def main():
 
     m = calculate_multiplier(max_daily_commits)
 
-    repo = request_user_input(
-        'Enter the name of the repository to use by gitfiti: ')
+    repo = request_user_input("Enter the name of the repository to use by gitfiti: ")
 
     offset = request_user_input(
-        'Enter the number of weeks to offset the image (from the left): ')
+        "Enter the number of weeks to offset the image (from the left): "
+    )
 
     offset = int(offset) if offset.strip() else 0
 
-    print((
-        'By default gitfiti.py matches the darkest pixel to the highest\n'
-        'number of commits found in your GitHub commit/activity calendar,\n'
-        '\n'
-        'Currently this is: {0} commits\n'
-        '\n'
-        'Enter the word "gitfiti" to exceed your max\n'
-        '(this option generates WAY more commits)\n'
-        'Any other input will cause the default matching behavior'
-    ).format(max_daily_commits))
+    print(
+        (
+            "By default gitfiti.py matches the darkest pixel to the highest\n"
+            "number of commits found in your GitHub commit/activity calendar,\n"
+            "\n"
+            "Currently this is: {0} commits\n"
+            "\n"
+            'Enter the word "gitfiti" to exceed your max\n'
+            "(this option generates WAY more commits)\n"
+            "Any other input will cause the default matching behavior"
+        ).format(max_daily_commits)
+    )
     match = request_user_input()
 
-    match = m if (match == 'gitfiti') else 1
+    match = m if (match == "gitfiti") else 1
 
-    print('Enter file(s) to load images from (blank if not applicable)')
-    img_names = request_user_input().split(' ')
+    print("Enter file(s) to load images from (blank if not applicable)")
+    img_names = request_user_input().split(" ")
 
     loaded_images = load_images(img_names)
     images = dict(IMAGES, **loaded_images)
 
-    print('Enter the image name to gitfiti')
-    print('Images: ' + ', '.join(images.keys()))
+    print("Enter the image name to gitfiti")
+    print("Images: " + ", ".join(images.keys()))
     image = request_user_input()
 
     image_name_fallback = FALLBACK_IMAGE
@@ -487,23 +498,29 @@ def main():
     fake_it_multiplier = m * match
 
     if not ghe:
-        git_url = 'git@github.com'
+        git_url = "git@github.com"
     else:
-        git_url = request_user_input('Enter Git URL like git@site.github.com: ')
-        
-    shell = ''
-    while shell not in SHELLS.keys(): 
+        git_url = request_user_input("Enter Git URL like git@site.github.com: ")
+
+    shell = ""
+    while shell not in SHELLS.keys():
         shell = request_user_input(
-            'Enter the target shell ({}): '.format(' or '.join(SHELLS.keys())))
+            "Enter the target shell ({}): ".format(" or ".join(SHELLS.keys()))
+        )
 
-    output = fake_it(image, start_date, username, repo, git_url, shell, offset,
-                     fake_it_multiplier)
+    output = fake_it(
+        image, start_date, username, repo, git_url, shell, offset, fake_it_multiplier
+    )
 
-    output_filename = 'gitfiti.{}'.format(SHELLS[shell])
+    output_filename = "gitfiti.{}".format(SHELLS[shell])
     save(output, output_filename)
-    print('{} saved.'.format(output_filename))
-    print('Create a new(!) repo named {0} at {1} and run the script'.format(repo, git_base))
+    print("{} saved.".format(output_filename))
+    print(
+        "Create a new(!) repo named {0} at {1} and run the script".format(
+            repo, git_base
+        )
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
